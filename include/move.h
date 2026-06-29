8cd33db31eea48b4edc7c3ff1447f7ebf3c96f02
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-06-29 10:41:37 +0545

    update

--- include/move.h
+++ include/move.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include "piece.h"
+
 struct Move {
     int fromRow;
     int fromCol;
 
     int toRow;
     int toCol;
+
+    bool capture;
+    bool doublePawnPush;
+    bool enPassant;
+    bool castleKingside;
+    bool castleQueenside;
+
+    Piece promotion;
 };
