c3c78451635c5d780e0e8f329128ffa2a914887f
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-28 05:19:18 +0545

    Update __init__.py

--- engine/__init__.py
+++ engine/__init__.py
@@ -4,5 +4,11 @@ A UCI-compliant chess engine
 Author: Rajesh Thapa (bokshi)
 """
 
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 __author__ = "Rajesh Thapa (bokshi)"
+
+from .board import Board
+from .search import Search
+from .uci import UCI
+from .perft import Perft
+from .transposition import TranspositionTable
