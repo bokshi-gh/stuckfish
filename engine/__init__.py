b59bb05d1d3419ddd26e8e3c77c96518f3481799
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-28 10:49:57 +0545

    Update __init__.py

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,7 +1,6 @@
 """
 Stuckfish Chess Engine
 A UCI-compliant chess engine
-Author: Rajesh Thapa (bokshi)
 """
 
 from .constants import ENGINE_NAME, ENGINE_VERSION, ENGINE_AUTHOR
@@ -9,6 +8,7 @@ from .board import Board
 from .search import Search
 from .uci import UCI
 from .transposition import TranspositionTable
+from .zobrist import Zobrist
 
 __version__ = ENGINE_VERSION
 __author__ = ENGINE_AUTHOR
